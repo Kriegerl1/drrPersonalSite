ab9eab2a7f921a89979df535602bba70569fa085
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-02-12 00:24:58 -0300

    Access counter

--- assets/js/js.js
+++ assets/js/js.js
@@ -16,3 +16,27 @@ function blurProduct(){
         hide.classList.add('hidden');
     }
 }
+
+// contador de acessos teste
+
+function setCookie(nome, valor, dias){ 
+    diasms = (new Date()) .getTime() + 1000 * 3600 * 24 * dias; 
+    dias = new Date(diasms); 
+    expires = dias.toGMTString(); 
+    document.cookie = escape(nome) + "=" + escape(valor) + "; expires=" + expires; 
+    } 
+
+if (!document.cookie){
+    setCookie("cookie", "1", 365);
+    document.write("Access Counter : 1");
+    } else {
+    var cont = document.cookie;
+    var dividindo = cont.split("=");
+    var numero = parseInt(dividindo[1]);
+    var soma = numero + 1;
+    document.write("Access Counter : " + soma);
+    setCookie("cookie", soma, 365);
+    }
+
+
+// contador de acessos teste
